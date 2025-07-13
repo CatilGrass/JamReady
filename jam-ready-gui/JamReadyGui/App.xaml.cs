715996c21552a56737ebfdba43b7f4d80c7af636
1992414357@qq.com <1992414357@qq.com>
2025-07-13 18:28:08 +0800

    提交GUI部分代码

---
+++ jam-ready-gui/JamReadyGui/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Windows;
+
+namespace JamReadyGui
+{
+    public partial class App
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            MainWindow window = new MainWindow();
+            // window.Show();
+            
+            Console.WriteLine(AppDomain.CurrentDomain.BaseDirectory);
+            
+        }
+    }
+}
