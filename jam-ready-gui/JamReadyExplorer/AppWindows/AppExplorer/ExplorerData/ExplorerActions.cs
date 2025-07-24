24cefa3d494a0fccd9916a8764909f66a063aac4
Weicao-CatilGrass <1992414357@qq.com>
2025-07-24 20:38:15 +0800

    修改了右键菜单的行为

--- jam-ready-gui/JamReadyExplorer/AppWindows/AppExplorer/ExplorerData/ExplorerActions.cs
+++ jam-ready-gui/JamReadyExplorer/AppWindows/AppExplorer/ExplorerData/ExplorerActions.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
 using System.Windows.Media.Imaging;
@@ -26,63 +27,75 @@ public static class ExplorerActions
         // 遍历所有菜单
         foreach (var pathMenu in ExplorerRegistry.PathMenus)
         {
-            var pathMenuItems = pathMenu.OnRegisterOperation(path);
-            if (pathMenuItems != null)
+            var explorerPath = ExplorerPath.FromString(path);
+            if (explorerPath != null)
             {
-                // 建立一级菜单
-                var menuItemMain = new MenuItem
+                var pathMenuItems = pathMenu.OnRegisterOperation(explorerPath.Value);
+                if (pathMenuItems != null)
                 {
-                    Header = pathMenu.GetMenuName().Trim(), Command = new ActionCommand(() =>
+                    // 建立一级菜单
+                    var menuItemMain = new MenuItem
                     {
-                        // 建立二级菜单
-                        var menuSub = new ContextMenu();
-                        int i = 0;
-                        foreach (var pathMenuItem in pathMenuItems)
+                        Header = pathMenu.GetMenuName().Trim(), Command = new ActionCommand(() =>
                         {
-                            int insert = i;
-                            var menuItemSub = new MenuItem
+                            // 子项数量
+                            int subItemCount = 0;
+                            
+                            // 建立二级菜单
+                            var menuSub = new ContextMenu();
+                            foreach (var pathMenuItem in pathMenuItems)
                             {
-                                Header = pathMenuItem.Trim(), Command = new ActionCommand(() =>
+                                int insert = subItemCount;
+                                var menuItemSub = new MenuItem
                                 {
-                                    var explorerPath = ExplorerPath.FromString(path);
-                                    if (explorerPath != null)
+                                    Header = pathMenuItem.Trim(), Command = new ActionCommand(() =>
                                     {
                                         if (pathMenu.OnOperate(explorerPath.Value, insert))
                                             explorer.RefreshExplorerItems();
-                                    }
-                                })
-                            };
-                            var iconSub = pathMenu.GetOperationIcon(path, i);
-                            if (iconSub != null)
-                            {
-                                menuItemSub.Icon = new Image
-                                {
-                                    Source = new BitmapImage(iconSub.Path)
+                                    })
                                 };
+                                var iconSub = pathMenu.GetOperationIcon(explorerPath.Value, subItemCount);
+                                if (iconSub != null)
+                                {
+                                    menuItemSub.Icon = new Image
+                                    {
+                                        Source = new BitmapImage(iconSub.Path)
+                                    };
+                                }
+                                
+                                // 添加子项，增加子项计数
+                                menuSub.Items.Add(menuItemSub);
+                                subItemCount++;
                             }
-                            menuSub.Items.Add(menuItemSub);
-                            i++;
-                        }
-                        menuSub.Placement = PlacementMode.Mouse;
-                        menuSub.IsOpen = true;
-                    })
-                };
-
-                var icon = pathMenu.GetIcon();
-                if (icon != null)
-                {
-                    menuItemMain.Icon = new Image
-                    {
-                        Source = new BitmapImage(icon.Path)
+                            menuSub.Placement = PlacementMode.Mouse;
+                            menuSub.IsOpen = true;
+                            menuSub.HorizontalOffset = menuSub.ActualWidth * 0.7;
+                            menuSub.VerticalOffset = - 16;
+                        })
                     };
-                }
-                menu.Items.Add(menuItemMain);
+
+                    // 子项必须至少有一项才会添加该主菜单
+                    if (pathMenuItems.Count > 0)
+                    {
+                        var icon = pathMenu.GetIcon();
+                        if (icon != null)
+                        {
+                            menuItemMain.Icon = new Image
+                            {
+                                Source = new BitmapImage(icon.Path)
+                            };
+                        }
+                        menu.Items.Add(menuItemMain);
+                    }
+                }                      
             }
         }
         
         if (menu.Items.Count > 0) {
             menu.Placement = PlacementMode.Mouse;
             menu.IsOpen = true;
+            menu.HorizontalOffset = menu.ActualWidth * 0.7;
+            menu.VerticalOffset = - 16;
         }
     }
 }
