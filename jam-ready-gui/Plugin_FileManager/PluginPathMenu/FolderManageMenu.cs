0034f4b2d5d79cb323618de11de461659f4da68a
Weicao-CatilGrass <1992414357@qq.com>
2025-07-24 20:33:59 +0800

    增加了文件夹的创建、删除功能

---
+++ jam-ready-gui/Plugin_FileManager/PluginPathMenu/FolderManageMenu.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using JamReadyGui.AppData.Explorer;
+using JamReadyGui.AppData.Utils;
+using Plugin_FileManager.PluginWindows;
+
+namespace Plugin_FileManager.PluginPathMenu;
+
+public class FolderManageMenu : PathMenu
+{
+    public override string GetMenuName()
+    {
+        return ExplorerRuntime.Lang(Plugin.PluginName, "menu_name");
+    }
+
+    public override List<string>? OnRegisterOperation(ExplorerPath path)
+    {
+        List<string> menuItemList = new();
+        Do(path, _ =>
+        {
+            menuItemList.Add(ExplorerRuntime.Lang(Plugin.
+                PluginName, "create_folder"));
+        });
+        return menuItemList;
+    }
+
+    public override bool OnOperate(ExplorerPath path, int operationIndex)
+    {
+        var changed = false;
+        Do(path, d =>
+        {
+            if (operationIndex == 0)
+            {
+                new CreateFolderWindow(d).Show();
+            }
+        });
+        return changed;
+    }
+
+    private void Do(ExplorerPath path, Action<DirectoryInfo> doOperation)
+    {
+        if (path.Prefix == Plugin.PluginPrefix && path.Path.Trim() != "_")
+        {
+            var dir = new DirectoryInfo(path.Path);
+            if (dir.Exists)
+            {
+                doOperation.Invoke(dir);
+            }
+        }
+    }
+}
