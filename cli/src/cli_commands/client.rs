5b5ae81f62b4b0b85263953b341d74b2c64a8cdd
Weicao-CatilGrass <1992414357@qq.com>
2025-08-05 11:31:12 +0800

    添加功能: 本地文件安全移动

--- cli/src/cli_commands/client.rs
+++ cli/src/cli_commands/client.rs
@@ -9,9 +9,10 @@ use colored::Colorize;
 use jam_ready::utils::address_str_parser::parse_address_v4_str;
 use jam_ready::utils::file_digest::md5_digest;
 use jam_ready::utils::local_archive::LocalArchive;
-use jam_ready::utils::text_process::parse_colored_text;
+use jam_ready::utils::text_process::{parse_colored_text, process_path_text};
 use std::env::{args, current_dir};
 use std::ops::Add;
+use jam_ready::utils::file_operation::move_file;
 
 /// 客户端命令行
 #[derive(Parser, Debug)]
@@ -341,9 +342,9 @@ struct MoveArgs {
     #[arg(long, short = 'g', alias = "lock")]
     get: bool,
 
-    // 自动同步工作区
-    #[arg(long, short = 'u', alias = "update")]
-    update: bool
+    // 仅移动本地文件
+    #[arg(long, short = 'l')]
+    local: bool
 }
 
 /// 回滚参数
@@ -463,11 +464,15 @@ pub async fn client_workspace_main() {
                 // 获得文件的锁
                 client_execute_command(vec!["file".to_string(), "get".to_string(), (&args.from_search).clone()]).await;
             }
-            // 移动文件
-            client_execute_command(vec!["file".to_string(), "move".to_string(), args.from_search, args.to_path]).await;
-            if args.update {
-                // 更新工作区
-                client_execute_command(vec!["update".to_string()]).await
+
+            if args.local {
+                // 移动本地文件
+                client_move_local_file(args).await;
+
+            } else {
+
+                // 移动远程文件
+                client_execute_command(vec!["file".to_string(), "move".to_string(), args.from_search, args.to_path]).await;
             }
         },
 
@@ -540,6 +545,45 @@ pub async fn client_workspace_main() {
 
         // 格洛克？？？
         ClientCommands::Glock => print_glock_xd(),
+    }
+}
+
+/// 移动本地文件
+async fn client_move_local_file(mv: MoveArgs) {
+    let mut local = LocalFileMap::read().await;
+    let database = Database::read().await;
+    let Some(local_file_mut) = local.search_to_local_mut(&database, mv.from_search.clone()) else {
+        eprintln!("Fail to move local file: '{}'", &mv.from_search);
+        return;
+    };
+    let Ok(current_dir) = current_dir() else { return; };
+
+    let raw_path = current_dir.join(&local_file_mut.local_path);
+    let target_path = current_dir.join(mv.to_path.clone());
+
+    if ! target_path.exists() {
+
+        // 修改本地文件地址
+        let new_path = process_path_text(mv.to_path);
+        let old_path = local_file_mut.local_path.clone();
+        local_file_mut.local_path = new_path.clone();
+
+        // 移除 Uuid
+        let Some(uuid) = local.file_uuids.remove(&old_path) else { return; };
+
+        // 重新绑定 Uuid 到新地址
+        local.file_uuids.insert(new_path, uuid);
+
+        // 全部成功后，移动文件
+        if let Ok(_) = move_file(&raw_path, &target_path) {
+
+            // 移动文件成功后，更新配置
+            LocalFileMap::update(&local).await;
+            println!("Moved local file '{}' to '{}'", raw_path.display(), target_path.display());
+        }
+
+    } else {
+        eprintln!("Cannot move file: file '{}' exists.", target_path.display());
     }
 }
 
