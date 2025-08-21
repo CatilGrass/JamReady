6eaf15b40b6c2d415211e6ad539e815806109ce8
魏曹先生 <1992414357@qq.com>
2025-08-22 03:19:05 +0800

    Modify the client help section documentation.

--- cli/src/help/help_docs.rs
+++ cli/src/help/help_docs.rs
@@ -41,10 +41,6 @@ ONLINE COMMANDS:
         Change version of Virtual File(s).
         [green]Learn more:[/] [yellow]jam[/] doc [cyan]rollback[/]
 
-    [yellow]complete[/]  [green]<FROM_SEARCH> <INFO?>[/]
-        Alias: cmpl, c, done, d
-        Mark the Local File(s) as [cyan]\"completed\"[/] and make it ready for [yellow]commit[/].
-
     [yellow]commit[/]
         Alias: cmt, save, sv
         Upload all held and modified Local File(s).
@@ -58,19 +54,23 @@ ONLINE COMMANDS:
         Alias: red
         Redirect to a new network address.
 
-    [yellow]update[/]    [yellow]database/struct[/]
+    [yellow]update[/]    [yellow]--database/--struct[/]
         Alias: sync
         Sync the local workspace struct with the remote.
 
 OFFLINE COMMANDS:
 
-    [yellow]query[/] [green]<QUERY_TYPE?> <QUERY_ITEMS?>[/]
+    [yellow]query[/]     [green]<QUERY_TYPE?> <QUERY_ITEMS?>[/]
         Alias: q
         Query some data.
 
-    [yellow]param[/] [green]<KEY> <VALUE?>[/]
+    [yellow]param[/]     [green]<KEY> <VALUE?>[/]
         Alias: set.
         Query or set a param.
+
+    [yellow]complete[/]  [green]<FROM_SEARCH> <INFO?>[/]
+        Alias: cmpl, c, done, d
+        Mark the Local File(s) as [cyan]\"completed\"[/] and make it ready for [yellow]commit[/].
 ";
 
 /// From ./_text/move.txt
@@ -184,17 +184,17 @@ ARGUMENTS: [green]<FROM_SEARCH>[/]  Downloaded and viewed files
 
 pub fn get_help_docs(name: &str) -> String {
     parse_colored_text(match name {
-        "struct" => STRUCT,
-        "ownership" => OWNERSHIP,
-        "server_help" => SERVER_HELP,
-        "view" => VIEW,
-        "setup_help" => SETUP_HELP,
         "search_rule" => SEARCH_RULE,
-        "rollback" => ROLLBACK,
-        "add" => ADD,
-        "move" => MOVE,
+        "server_help" => SERVER_HELP,
+        "setup_help" => SETUP_HELP,
+        "struct" => STRUCT,
+        "view" => VIEW,
         "remove" => REMOVE,
+        "move" => MOVE,
+        "ownership" => OWNERSHIP,
         "client_help" => CLIENT_HELP,
+        "add" => ADD,
+        "rollback" => ROLLBACK,
         _ => "",
     }.trim())
 }
