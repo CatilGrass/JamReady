0c6523156f7e4af55478e8dafe5abe0b99505738
魏曹先生 <1992414357@qq.com>
2025-08-24 12:37:09 +0800

    Move try_correct_current_dir to lib.rs

--- cli/src/bin/jam.rs
+++ cli/src/bin/jam.rs
@@ -1,32 +1,8 @@
-use std::env::{current_dir, set_current_dir};
-use std::path::PathBuf;
 use jam_ready_cli::cli_commands::setup::cli_entry;
+use jam_ready_cli::try_correct_current_dir;
 
 #[tokio::main]
 async fn main() {
-    try_correct_current_dir();
+    let _ = try_correct_current_dir();
     cli_entry().await;
-}
-
-/// Attempt to correct the current directory
-fn try_correct_current_dir() {
-    let current = current_dir().unwrap();
-    if current.join(env!("PATH_WORKSPACE_ROOT")).exists() {
-        return;
-    } else {
-        if let Some(found) = check_parent(current) {
-            set_current_dir(found).unwrap();
-        }
-    }
-    fn check_parent(current: PathBuf) -> Option<PathBuf> {
-        if current.join(env!("PATH_WORKSPACE_ROOT")).exists() {
-            Some(current)
-        } else {
-            if let Some(parent) = current.parent() {
-                check_parent(PathBuf::from(parent))
-            } else {
-                None
-            }
-        }
-    }
 }
--- cli/src/lib.rs
+++ cli/src/lib.rs
@@ -2,3 +2,33 @@ pub mod data;
 pub mod service;
 pub mod cli_commands;
 pub mod help;
+pub mod linker;
+
+use std::env::current_dir;
+use std::env::set_current_dir;
+use std::io::ErrorKind::NotFound;
+use std::path::PathBuf;
+
+/// Attempt to correct the current directory
+pub fn try_correct_current_dir() -> Result<(), std::io::Error> {
+    let current = current_dir()?;
+    return if current.join(env!("PATH_WORKSPACE_ROOT")).exists() {
+        Ok(())
+    } else {
+        if let Some(found) = check_parent(current) {
+            set_current_dir(found)?;
+        }
+        Err(std::io::Error::new(NotFound, "Workspace directory does not exist."))
+    };
+    fn check_parent(current: PathBuf) -> Option<PathBuf> {
+        if current.join(env!("PATH_WORKSPACE_ROOT")).exists() {
+            Some(current)
+        } else {
+            if let Some(parent) = current.parent() {
+                check_parent(PathBuf::from(parent))
+            } else {
+                None
+            }
+        }
+    }
+}
